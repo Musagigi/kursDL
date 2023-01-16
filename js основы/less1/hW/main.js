9557cf0d4ae0b2413fa579d19c0f22dea9be9115
Musajiji <dzhal@bk.ru>
2023-01-16 21:22:20 +0300

    edit js hW less1

--- js основы/less1/hW/main.js
+++ js основы/less1/hW/main.js
@@ -1,18 +1,46 @@
 window.addEventListener('load', function () {
 
 	let numOne = document.querySelector('.inpNumbOne')
-	let selSign = document.querySelector('.selectSign')
 	let numTwo = document.querySelector('.inpNumbTwo')
+	let selSign = document.querySelector('.selectSign')
 	let btnAns = document.querySelector('.btnAns')
-	let answer = document.querySelector('.answer')
-	console.log(numOne, selSign, numTwo, btnAns, answer);
+	let result = document.querySelector('.result')
 
-	btnAns.addEventListener('click', numbers)
+	console.log(numOne, selSign, numTwo, btnAns, result);
 
-})
+	btnAns.addEventListener('click', calculate)
 
-function numbers() {
-	if (selSign.option.value == 'plus') {
-		answer = +numOne + +numTwo
+	function calculate() {
+		if (numOne.value === '' && numTwo.value === '') {
+			return
+		}
+		if (selSign.value === 'plus') {
+			result.innerText = +numOne.value + +numTwo.value
+			return this.disabled = true
+		}
+		if (selSign.value === 'minus') {
+			result.innerText = +numOne.value - +numTwo.value
+			return this.disabled = true
+		}
+		if (selSign.value === 'multi') {
+			result.innerText = +numOne.value * +numTwo.value
+			return this.disabled = true
+		}
+		if (selSign.value === 'dvsin') {
+			result.innerText = +numOne.value / +numTwo.value
+			return this.disabled = true
+		}
 	}
-}
+
+	numOne.addEventListener('keydown', changeBtn)
+	numTwo.addEventListener('keydown', changeBtn)
+
+	function changeBtn() {
+		this.value = this.value.replace(/\D/g, '')
+		if (this) { return btnAns.disabled = false }
+	}
+
+	// if (selSign.value === 'minus') { return result.innerText = num1 - num2 }
+	// 	if (selSign.value === 'multi') { return result.innerText = num1 * num2 }
+	// 	return result.innerText = num1 / num2
+})
