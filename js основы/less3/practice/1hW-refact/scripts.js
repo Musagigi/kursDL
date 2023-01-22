4b31f9485d1ce1d411728f6200a7621da110a747
Musajiji <dzhal@bk.ru>
2023-01-22 15:05:29 +0300

    22/01/23 js practice

--- js основы/less3/practice/1hW-refact/scripts.js
+++ js основы/less3/practice/1hW-refact/scripts.js
@@ -1,30 +1,33 @@
 window.addEventListener('load', function () {
 
 
-	let item = document.querySelectorAll('.check')
-	let form = document.querySelector('form')
+	let form = document.querySelector('.form') // находим во всем документе селектор form
+	let itemInput = form.querySelectorAll('.check') //ищем в рамках узла form
 
 
 	form.addEventListener('click', function (event) {
 
-		let inp = event.target.classList.contains('check')
-		let sbmt = event.target.classList.contains('sbmt')
+		// let inp = event.target.classList.contains('check')
+		// let sbmt = event.target.classList.contains('sbmt')
+		let hasError = false
 
-		if (sbmt) {
-			for (let i = 0; i < item.length; i++) {
-				if (item[i].value == '') {
-					item[i].classList.add('errorColor')
-					event.preventDefault()
+		if (event.target.classList.contains('sbmt')) {
+			for (let i = 0; i < itemInput.length; i++) {
+				if (itemInput[i].value == '') {
+					itemInput[i].classList.add('errorColor')
+					hasError = true
 				}
-				else if (item[i].value != '') {
-					item[i].classList.remove('errorColor')
+				else if (itemInput[i].value != '') {
+					itemInput[i].classList.remove('errorColor')
 				}
 			}
+			if (hasError) { event.preventDefault() }
 
 		}
-		if (inp) {
+		if (event.target.classList.contains('check')) {
 			event.target.classList.remove('errorColor');
-
 		}
 	})
 })
+
+// for of - перебор итерируемой сущности у сущности запускается Symbol.iterator
