08f0036177634e82eba83fc41011acce2f63c7ce
Musagigi <dzhal@bk.ru>
2023-03-10 17:55:04 +0300

    add dir3

--- js основы/less5/hW/3/js/script.js
+++ js основы/less5/hW/3/js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
 
 	let btnPrev = document.querySelector('.gallery .buttons .prev');
 	let btnNext = document.querySelector('.gallery .buttons .next');
@@ -6,26 +6,40 @@ window.addEventListener('load', function(){
 	let images = document.querySelectorAll('.gallery .photos img');
 	let i = 0;
 
-	btnPrev.addEventListener('click', function(){
+	btnPrev.addEventListener('click', function () {
+
 		images[i].classList.remove('showed');
 		i--;
 
-		if(i < 0){
+		if (i < 0) {
 			i = images.length - 1;
 		}
 
+
 		images[i].classList.add('showed');
+
+		images[i].animate([
+			{ transform: 'scale(0)' },
+			{ transform: 'scale(1)' }
+		], { duration: 1000 });
 	});
 
-	btnNext.addEventListener('click', function(){
+	btnNext.addEventListener('click', function () {
+
 		images[i].classList.remove('showed');
 		i++;
+		images[i].animate([
+			{ transform: 'scale(1)' },
+			{ transform: 'scale(0)' }
+		], { duration: 1000 });
 
-		if(i >= images.length){
+		if (i >= images.length) {
 			i = 0;
 		}
 
 		images[i].classList.add('showed');
+
+
 	});
 
 });
