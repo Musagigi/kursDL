032b775437ef01d8a4cf819b759f7a28bafed106
Musajiji <dzhal@bk.ru>
2023-07-03 07:50:48 +0300

    add multiple pages

--- верстка23/less4grid&vite/native/vite.config.js
+++ верстка23/less4grid&vite/native/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
+const { resolve } = require('path')
+
 
 export default defineConfig({
 	root: './src',
 	build: {
 		outDir: './../dist', //  путь относительно root
-		emptyOutDir: true
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: resolve('./src/index.html'),
+				nested: resolve('./src/pages/posts.html')
+			}
+		}
 	},
 	publicDir: './../public'
 })
